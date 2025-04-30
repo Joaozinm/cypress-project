26c959d3b057cf8a9d52a5368526395d4e815c0f
Joao Melo <vivitomelo@gmail.com>
2025-04-30 00:15:56 -0300

    feat: base Url

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,12 +1,6 @@
 import userData from "../fixtures/users/userData.json";
 
 describe("Orange HRM Tests", () => {
-  const urls = {
-    indexPath:
-      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
-    homePath: "/web/index.php/dashboard/index",
-  };
-
   const selectorsList = {
     usernameField: "[name='username']",
     passwordField: "[name='password']",
@@ -16,15 +10,15 @@ describe("Orange HRM Tests", () => {
   };
 
   it("Login - Success", () => {
-    cy.visit(urls.indexPath);
+    cy.visit("/auth/login");
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username);
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password);
     cy.get(selectorsList.submitButton).click();
-    cy.location("pathname").should("eq", urls.homePath);
+    cy.location("pathname").should("eq", "/web/index.php/dashboard/index");
     cy.get(selectorsList.dashboardGrid).should("be.visible");
   });
   it("Login - Fail", () => {
-    cy.visit(urls.indexPath);
+    cy.visit("/auth/login");
     cy.get(selectorsList.usernameField).type(userData.userFail.username);
     cy.get(selectorsList.passwordField).type(userData.userFail.password);
     cy.get(selectorsList.submitButton).click();
